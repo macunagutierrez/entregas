aaf6fc5f545bba8386ee196b6f173607e6e4157a
macunagutierrez <miguel.acuna@inetum.com>
2024-04-27 18:24:55 +0200

    Practica 1 Ejercicio Jenkins 2

---
+++ practica1/jenkins2/scriptUnitarias copy.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            } 
+        }
+        stage('GIT') {
+            steps {
+                echo 'Download git repository'
+                git 'https://github.com/macunagutierrez/helloworld.git'
+            } 
+        }
+         stage('Comprobaciones') {
+            steps {
+                echo 'Comprobaciones de descarga de repositorio'
+                echo WORKSPACE
+                bat 'dir'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Stage Build no hace nada'
+            }
+        }
+        stage('TEST unitarios') {
+            steps {
+                bat '''
+                    set PYTHONPATH=%WORKSPACE%
+                    python --version
+                    python -m pytest test/unit
+                '''
+            }
+        }
+    }
+}
